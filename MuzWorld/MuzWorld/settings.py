ad97da63efb77f1b063dd0974f4732057d8f9824
Regina Gilyazova <reginaregiska@gmail.com>
2015-06-20 14:33:49 +0300

    Changes with registration/models.py were added

--- MuzWorld/MuzWorld/settings.py
+++ MuzWorld/MuzWorld/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'registration',
+	'users',
 )
 
 MIDDLEWARE_CLASSES = (
--- MuzWorld/registration/models.py
+++ MuzWorld/registration/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+	user_login = models.CharField(max_length = 50)
+	user_name = models.CharField(max_length = 50)
+	user_email = models.CharField(max_length = 100)
+	user_password = models.IntegerField()
+	def __str__(self):
+		return self.user_name
